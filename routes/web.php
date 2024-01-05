ddfe155929b52baa7fb9dcc1d0dcc867f3dc1efe
Angel Medrano G <medrano.angelg@gmail.com>
2024-01-04 19:21:56 -0600

    feat grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,29 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth', 'verified']);;
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth', 'verified']);
+
+Route::prefix('sistema')->middleware(['auth'])->group(function () {
+
+     //Asistencia Digital 
+     Route::prefix('atencion-digital')->group(function () {
+        //Route::resource('clientes', App\Http\Controllers\ClientesController::class)->names('clientes');
+    });
+
+     //Expediente Digital 
+     Route::prefix('gestion-ciudadana')->group(function () {
+        //Route::resource('clientes', App\Http\Controllers\ClientesController::class)->names('clientes');
+    });
+
+    //Expediente Digital 
+    Route::prefix('expediente-digital')->group(function () {
+        //Route::resource('clientes', App\Http\Controllers\ClientesController::class)->names('clientes');
+    });
+
+    //Expediente Digital 
+    Route::prefix('encuestas')->group(function () {
+        //Route::resource('clientes', App\Http\Controllers\ClientesController::class)->names('clientes');
+    });
+
+
+});
