84a4fba5e16a7aaddc22bfc6b9b6cfc4a6609741
Angel Medrano G <medrano.angelg@gmail.com>
2024-01-05 01:29:50 -0600

    verify token change

--- app/Http/Controllers/AD/WhatsAppController.php
+++ app/Http/Controllers/AD/WhatsAppController.php
@@ -11,20 +11,20 @@ class WhatsAppController extends Controller
     public function verifyToken(Request $request){
         try{
 
-            if($request->hub_challenge != null && $request->hub_verify_token != null && $request->hub_verify_token ==  env('WHATSAPP_TOKEN')){
-                return response($request->hub_challenge);
+          if($request->hub_challenge != null && $request->hub_verify_token != null && $request->hub_verify_token ==  env('WHATSAPP_TOKEN')){
+                 return response($request->hub_challenge);
             }
 
             return response()->json(['status' => 'error'], 400);
-           
+
         }catch(\Exception $e){
-            Log::error('ocurrio un verificar el token de WhatsApp: ' . $e->getMessage());
+            Log::error('ocurrio un error al verificar el token de WhatsApp: ' . $e->getMessage());
             return response()->json(['status' => 'error','errors' => $e->getMessage()], 400);
         }
     }
 
     public function receivedMessage(Request $request){
-		Log::info('recibiendo mensaje');
+		Log::info('Mensaje');
 		try{
 			Log::info('Mensaje Recibido: ' . json_encode($request->all()));
 			return response("EVENT_RECEIVED");
--- app/Livewire/Apps/AD/Subcategorias/SubcategoriasEdit.php
+++ app/Livewire/Apps/AD/Subcategorias/SubcategoriasEdit.php
@@ -2,12 +2,55 @@
 
 namespace App\Livewire\Apps\AD\Subcategorias;
 
+use App\Models\AdCategorias;
+use Exception;
+use Livewire\Attributes\Locked;
 use Livewire\Component;
 
-class SubcategoriasEdit extends Component
-{
-    public function render()
-    {
+class SubcategoriasEdit extends Component {
+
+    #[Locked]
+    public AdCategorias $categoria;
+
+    public $nombre, $tiempoRespuesta;
+
+    protected array $rules = [
+        'nombre'          => 'required|string',
+        'tiempoRespuesta' => 'required|numeric',
+    ];
+
+    protected array $validationAttributes = [
+        'tiempoRespuesta' => 'tiempo máximo de respuesta',
+    ];
+
+    public function mount(AdCategorias $categoria) {
+        $this->categoria = $categoria;
+    }
+
+    public function submit() {
+
+        $this->validate($this->rules, [], );
+
+        try {
+            
+            $data = new AdSubcategorias();
+            $data->nombre           = $this->nombre;
+            $data->tiempo_respuesta = $this->tiempoRespuesta;
+            $data->categoria_id     = $this->categoria->id;
+            $data->save();
+
+            toastr()->success('La subcategoría ha sido editada correctamente.', 'Subcategorías', ["positionClass" => "toast-top-right"]);
+            return response()->redirectToRoute('sistema.atencion-digital.subcategorias.index', $this->categoria->id);
+        }
+        catch(Exception $exception) {
+            
+            $this->dispatch('showAlertError', $exception->getMessage());
+        }
+
+    }
+
+    public function render() {
         return view('livewire.apps.a-d.subcategorias.subcategorias-edit');
     }
+
 }
